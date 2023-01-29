c33714603f77051389887ebb8f355457ef32c52e
Noel Lewis <53228596+nalewis57@users.noreply.github.com>
2023-01-29 12:05:19 -0500

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,4 +2,6 @@
 print "Hello, World! php version is " . PHP_VERSION ;
 print "A change is coming.";
 print "This is getting fun.";
+print "How cool is this?";
+print "Am beginning to like this.";
 ?>
